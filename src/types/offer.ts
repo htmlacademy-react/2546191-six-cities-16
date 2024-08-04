d893d5d727d2ccf2054e580c2c0688301a5bd348
AlexandeR <JasTeen@mail.ru>
2024-08-05 00:19:19 +0700

    fix linter

--- src/types/offer.ts
+++ src/types/offer.ts
@@ -2,7 +2,6 @@ import { Host } from './host';
 import { OfferPreview } from './offerPreview';
 
 
-
 export type Offer = Omit<OfferPreview, 'previewImage'> & {
     description: string;
     bedrooms: number;
